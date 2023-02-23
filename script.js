9102f8c21dfd1a580f377c4869ea6c492aac3b19
Jeevan Johal <jkjohal@ucdavis.edu>
2023-02-22 18:42:01 -0800

    added html for question choices and highscores

--- script.js
+++ script.js
@@ -1,36 +1,44 @@
-const quizQuestions = [{
-    id:0,
-    q: "One of the most popular Git repository hosts is called...",
-    a: [{ text: "Gethub", isCorrect: false },
+
+//quiz questions
+const quizQuestions = [
+    {
+        id:0,
+        q: "One of the most popular Git repository hosts is called...",
+        a: [
+            { text: "Gethub", isCorrect: false },
             { text: "Gothub", isCorrect: false },
             { text: "Github", isCorrect: true },
             { text: "Greathub", isCorrect: false },
             
-          ] 
+            ] 
     },
     
-    {id:1,
-    q: "CSS stands for...",
-    a: [{ text: "Cold salami sandwich", isCorrect: false },
+    {
+        id:1,
+        q: "CSS stands for...",
+        a: [
+            { text: "Cold salami sandwich", isCorrect: false },
             { text: "Cascading sheet styling", isCorrect: false },
             { text: "Cascading style sheets", isCorrect: true },
-            { text: "Computer science school", isCorrect: false }
-          ]
+            { text: "Computer science school", isCorrect: false },
+            ]
     },
 
-    {id:2,
+    {
+        id:2,
         q: "Which of the following will you NOT gain from a coding course?",
-        a: [{ text: "New skills", isCorrect: false },
-                { text: "Knowledge about coding", isCorrect: false },
-                { text: "Weight", isCorrect: true },
-                { text: "Troubleshooting abilities", isCorrect: false }
-        ]
+        a: [
+            { text: "New skills", isCorrect: false },
+            { text: "Knowledge about coding", isCorrect: false },
+            { text: "Weight", isCorrect: true },
+            { text: "Troubleshooting abilities", isCorrect: false },
+            ]
 
     }
 ]            
 
-
+//quiz logic
 const start = document.querySelector(#start-btn)
-const questionArea = document.querySelector(questions)
+const questionArea = document.querySelector(#questions)
 
 function startGame

48e960ecb03d34134012e31bb674ad3695336c13
Jeevan Johal <jkjohal@ucdavis.edu>
2023-02-23 13:55:43 -0800

    javascript for highscores page

---
+++ scores.js
@@ -0,0 +1,28 @@
+function showHighscores(){
+    var highscores = JSON.parse(window.localStorage.getItem("highscores")) || [];
+
+    highscores.sort(function (a,b){
+        return b.score - a.score;
+    });
+
+    for (var i = 0; i < highscores.length; i += 1) {
+
+        var liTag = document.createElement("li");
+        liTag.textContent = highscores[i].userInitials + " - " + highscores[i].score;
+
+        var olEl = document.querySelector("#highscores");
+        olEl.appendChild(liTag);
+    }
+};
+
+
+function clearScores(){
+    window.localStorage.removeItem('highscores');
+    window.location.reload();
+};
+
+var clearBtn = document.querySelector("#clear")
+
+clearBtn.addEventListener("click", clearScores);
+
+showHighscores();
